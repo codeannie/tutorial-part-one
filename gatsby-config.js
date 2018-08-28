4d88a5219cdbe82e5666d2ed9918b85c6fcabce5
Annie Chiang <devbear22@gmail.com>
2018-08-28 16:01:41 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // siteMetadata: {
+  //   title: `Gatsby`,
+  //   siteUrl: `https://www.gatsbyjs.org`,
+  //   description: `Blazing-fast static site generator for React`
+  // },
+  // Note: it must *not* have a trailing slash.
+  pathPrefix: `https://ssd.kp.org/news`
+  // pathPrefix: `/news`
+};
